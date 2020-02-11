aa231ffea5b2b95d879703810972f549f40126dc
BitYoungjae <bityoungjae@gmail.com>
2020-02-12 00:10:52 +0900

    set route handlers

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
-import express from 'express';
-import fs from 'fs';
+import fs, { promises } from 'fs';
 import path from 'path';
+import express from 'express';
 import { getPathNames } from 'esm-pathnames';
 
-const { __dirname, __filename } = getPathNames(import.meta);
+const { writeFile } = promises;
+const { __dirname } = getPathNames(import.meta);
 
 const app = express();
 
-// app.get('/', (req, res) => {
-//   res.status(200).send({
-//     message: 'Hello from the server side!',
-//     app: 'Natours',
-//   });
-// });
+// Use middlewares
+app.use(express.json());
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint!');
-// });
+// Global Variables
+const dataPath = path.resolve(__dirname, './dev-data/data/tours-simple.json');
+const tours = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
 
-const tours = JSON.parse(
-  fs.readFileSync(
-    path.resolve(__dirname, './dev-data/data/tours-simple.json'),
-    'utf-8',
-  ),
-);
-
+// Routes
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
     status: 'success',
@@ -35,6 +26,51 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params;
+  const tour = tours.find(el => el.id == id);
+
+  if (!tour) {
+    res.status(404 /*404 means Not Found*/).json({
+      status: 'fail',
+      data: `Not Found (#${id})`,
+    });
+
+    return;
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', async (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+
+  const newTour = {
+    id: newId,
+    ...req.body,
+  };
+
+  tours.push(newTour);
+
+  try {
+    await writeFile(dataPath, JSON.stringify(tours), 'utf8');
+    res.status(201 /* 201 means created */).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch {
+    res.end('Error Occured');
+  }
+});
+
+// Boiler Plate
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
